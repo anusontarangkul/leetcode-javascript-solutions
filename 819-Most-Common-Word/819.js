fa8be831f504332c307734f5aca056369d969df2
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-04-04 17:24:49 -0700

    add 819 solution

---
+++ 819-Most-Common-Word/819.js
@@ -0,0 +1,38 @@
+var mostCommonWord = function (paragraph, banned) {
+    let bannedSet = new Set(banned)
+    let map = {};
+    let wordArray = paragraph.toLowerCase().match(/[a-z]+/g)
+    for (let i = 0; i < wordArray.length; i++) {
+        if (!bannedSet.has(wordArray[i])) {
+            map[wordArray[i]] = (map[wordArray[i]] | 0) + 1;
+        }
+    }
+    let maxCount = 0;
+    let maxWord = "";
+
+    for (let key in map) {
+        if (map[key] > maxCount) {
+            maxWord = key;
+            maxCount = map[key];
+        }
+    }
+    return maxWord;
+};
+
+/*
+Time Complexity: O(n+m)
+Space Complexity: O(n+m)
+1. Create bannedSet from banned.
+2. Create map object.
+3. Create wordArray from paragraph.
+4. Loop through wordArray.
+    a. Condition if current word is not in the bannedSet.
+        i. Add it to the map object and increment the count.
+5. Create maxCount variable.
+6. Create maxWord variable.
+7. Loop through each in map.
+    a. Condition if the value is greater than the maxCount.
+        i. Change the maxWord to the map key value.
+        ii. Change the maxCount to the key value.
+8. Return maxWord.
+*/
