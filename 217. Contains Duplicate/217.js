21bfd0f032d2fc5c518e7949a477f8a5250375be
David Anusontarangkul <anusontarangkul.d@gmail.com>
2022-08-21 20:39:17 -0700

    217

---
+++ 217. Contains Duplicate/217.js
@@ -0,0 +1,20 @@
+var containsDuplicate = function (nums) {
+    const numSet = new Set();
+    for (let i = 0; i < nums.length; i++) {
+        if (numSet.has(nums[i])) {
+            return true;
+        }
+        numSet.add(nums[i])
+    }
+    return false;
+};
+
+// time complexity: O(n) where n is the length of nums
+// space complexity: O(n) where n is the length of nums
+
+// 1. Create a new numSet.
+// 2. Loop through nums
+//      a. If numSet has current num.
+//          i. Return true.
+//      b. Add current num to numSet.
+// 3. Return false
